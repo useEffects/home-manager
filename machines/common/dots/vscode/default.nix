1faaad27a5fc304c05d17e0bbc3203dfc3186730
useEffects <joel.sr1024@gmail.com>
2024-10-13 12:09:29 +0530

    add arv.json to vscode

--- machines/common/dots/vscode/default.nix
+++ machines/common/dots/vscode/default.nix
@@ -15,5 +15,8 @@
     "${config.xdg.configHome}/Code/User/settings.json" = {
       source = ./. + "/settings.json";
     };
+    "${config.home.homeDirectory}/.vscode/argv.json" = {
+      source = ./. + "/argv.json";
+    };
   };
 }
